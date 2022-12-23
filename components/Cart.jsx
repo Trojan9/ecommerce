7265f6c90c0df4a65b273804b0a8d5dda45f2393
OLUWATIMILEHIN BANJO <TIMMYBANJO@GMAIL.COM>
2022-12-23 11:31:46 +0100

    update

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -68,7 +68,7 @@ const Cart = () => {
               <div className="item-desc">
                 <div className="flex top">
                   <h5>{item.name}</h5>
-                  <h4>${item.price}</h4>
+                  <h4><span>&#8358;</span>{item.price}</h4>
                 </div>
                 <div className="flex bottom">
                   <div>
@@ -96,7 +96,7 @@ const Cart = () => {
           <div className="cart-bottom">
             <div className="total">
               <h3>Subtotal:</h3>
-              <h3>${totalPrice}</h3>
+              <h3><span>&#8358;</span>{totalPrice}</h3>
             </div>
             <div className="btn-container">
               <button type="button" className="btn" onClick={async ()=>{
